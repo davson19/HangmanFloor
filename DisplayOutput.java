5d1a4665ffad026f64840db3e25f69ab83f98f91
micpin19 <micpin19@gmail.com>
2017-06-06 13:04:30 -0400

    STUFF WORKS (KINDA)

--- DisplayOutput.java
+++ DisplayOutput.java
@@ -1,6 +1,7 @@
 package HangmanFloor;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DisplayOutput extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -8,13 +9,24 @@ public class DisplayOutput extends JFrame {
 
 	public static void main(String[] args) throws InterruptedException {
 		DisplayOutput frame = new DisplayOutput();
+        frame.setLayout(new GridBagLayout());
 
-//        HangmanPanel p = new HangmanPanel();
-//        frame.add(p);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        HangmanPanel p = new HangmanPanel();
+        p.setPreferredSize(new Dimension(192 / 2, 192 / 2));
+        frame.add(p, c);
+        c.gridx = 1;
+        WordsPanel wp = new WordsPanel("Hello World");
+        wp.setPreferredSize(new Dimension(192 / 2, 192 / 2));
+        frame.add(wp, c);
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 2;
         KeyboardPanel kp = new KeyboardPanel(false);
-        frame.add(kp);
-//        WordsPanel wp = new WordsPanel("Hello World");
-//        frame.add(wp);
+        kp.setPreferredSize(new Dimension(192, 192 / 2));
+        frame.add(kp, c);
 
 		frame.setTitle("Hangman!");
 		frame.setSize(192, 192);
