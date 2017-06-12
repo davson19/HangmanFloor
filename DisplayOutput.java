8793ec0efe907c07ab96867bc280c57c87adf949
micpin19 <micpin19@gmail.com>
2017-06-12 19:52:33 -0400

    PLEASE WORK

--- DisplayOutput.java
+++ DisplayOutput.java
@@ -10,6 +10,7 @@ public class DisplayOutput extends JFrame {
 	public static final int REFRESH = 80;
 
 	public static void main(String[] args) throws InterruptedException {
+
 	        Point[] pos = new Point[26];
 	        int count = 0;
             for (int j = 18; j < 21; j+=2) {
@@ -55,12 +56,11 @@ public class DisplayOutput extends JFrame {
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 
-			Scanner scan = new Scanner(System.in);
+
 
 			while (true) {
                 frame.repaint();
-				System.out.println("What is the word/phrase that will be used");
-				wp.setWord(scan.nextLine().toUpperCase());
+				wp.setWord(Dictionary.getWord());
 				wp.repaint();
                 boolean[][] arr = new boolean[0][];
 				try {
@@ -68,7 +68,7 @@ public class DisplayOutput extends JFrame {
 				} catch (Exception e) {
 					System.err.println("Error: Make sure that the board is turned on and plugged in.");
 				}
-				
+
 				while (!hp.gameOver() && !wp.isSolved()) {
 					int c = 0;
 
@@ -85,20 +85,20 @@ public class DisplayOutput extends JFrame {
 					}
 					Thread.sleep(REFRESH);
 				}
-				
+
 				GameOverPanel end = new GameOverPanel(true);
 				end.setPreferredSize(new Dimension(192, 192));
 				GridBagConstraints cg = new GridBagConstraints();
 				cg.gridx = 0;
 				cg.gridy = 0;
-				cg.gridwidth = 2;
-				cg.gridheight = 2;
+				cg.gridwidth = 1;
+				cg.gridheight = 1;
 				frame.remove(hp);
                 frame.remove(wp);
                 frame.remove(kp);
 				frame.add(end, cg);
-				end.repaint();
                 frame.repaint();
+                end.repaint();
                 Thread.sleep(5000);
                 frame.remove(end);
                 frame.add(hp, ch);
